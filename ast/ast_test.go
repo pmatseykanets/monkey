a8ec7baf7d9a52dec926a4c6fdbded949097bbb5
Peter Matseykanets <pmatseykanets@gmail.com>
2019-12-01 18:03:33 -0500

    Improve test coverage of ast package

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -27,3 +27,127 @@ func TestProgramString(t *testing.T) {
 		t.Errorf("Expected %s got %s", want, got)
 	}
 }
+
+func TestPrefixString(t *testing.T) {
+	tests := []struct {
+		prefix *Prefix
+		want   string
+	}{
+		{
+			&Prefix{
+				Token:    token.Token{Type: token.BANG, Literal: "!"},
+				Operator: "!",
+				Right: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "a"},
+					Value: "a",
+				},
+			},
+			"(!a)",
+		},
+		{
+			&Prefix{
+				Token:    token.Token{Type: token.MINUS, Literal: "-"},
+				Operator: "-",
+				Right: &Infix{
+					Token: token.Token{Type: token.PLUS, Literal: "+"},
+					Left: &IntegerLiteral{
+						Token: token.Token{Type: token.INT, Literal: "5"},
+						Value: 5,
+					},
+					Operator: "+",
+					Right: &Identifier{
+						Token: token.Token{Type: token.IDENT, Literal: "b"},
+						Value: "b",
+					},
+				},
+			},
+			"(-(5 + b))",
+		},
+	}
+
+	for _, tt := range tests {
+		if got := tt.prefix.String(); tt.want != got {
+			t.Errorf("Expected %s got %s", tt.want, got)
+		}
+	}
+}
+
+func TestInfixString(t *testing.T) {
+	tests := []struct {
+		infix *Infix
+		want  string
+	}{
+		{
+			&Infix{
+				Token: token.Token{Type: token.PLUS, Literal: "+"},
+				Left: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "a"},
+					Value: "a",
+				},
+				Operator: "+",
+				Right: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "b"},
+					Value: "b",
+				},
+			},
+			"(a + b)",
+		},
+		{
+			&Infix{
+				Token: token.Token{Type: token.PLUS, Literal: "*"},
+				Left: &IntegerLiteral{
+					Token: token.Token{Type: token.INT, Literal: "5"},
+					Value: 5,
+				},
+				Operator: "*",
+				Right: &Infix{
+					Token: token.Token{Type: token.PLUS, Literal: "+"},
+					Left: &Identifier{
+						Token: token.Token{Type: token.IDENT, Literal: "a"},
+						Value: "a",
+					},
+					Operator: "+",
+					Right: &Identifier{
+						Token: token.Token{Type: token.IDENT, Literal: "b"},
+						Value: "b",
+					},
+				},
+			},
+			"(5 * (a + b))",
+		},
+	}
+
+	for _, tt := range tests {
+		if got := tt.infix.String(); tt.want != got {
+			t.Errorf("Expected %s got %s", tt.want, got)
+		}
+	}
+}
+
+func TestBooleanString(t *testing.T) {
+	tests := []struct {
+		boolean *Boolean
+		want    string
+	}{
+		{
+			&Boolean{
+				Token: token.Token{Type: token.TRUE, Literal: "true"},
+				Value: true,
+			},
+			"true",
+		},
+		{
+			&Boolean{
+				Token: token.Token{Type: token.FALSE, Literal: "false"},
+				Value: false,
+			},
+			"false",
+		},
+	}
+
+	for _, tt := range tests {
+		if got := tt.boolean.String(); tt.want != got {
+			t.Errorf("Expected %s got %s", tt.want, got)
+		}
+	}
+}
